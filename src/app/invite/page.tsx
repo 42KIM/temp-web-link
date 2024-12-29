135c54c63948d25d195a4495f59962ca63b9783a
42KIM <ksahoo524@gmail.com>
2024-12-29 21:32:03 +0900

    feat: /invite로 진입 시 구글 플레이스토어로 redirect(임시)

---
+++ src/app/invite/page.tsx
@@ -0,0 +1,11 @@
+import {redirect} from "next/navigation";
+
+function InvitePage() {
+    redirect('https://play.google.com/store')
+
+    return (
+        <div>redirecting...</div>
+    )
+}
+
+export default InvitePage;
