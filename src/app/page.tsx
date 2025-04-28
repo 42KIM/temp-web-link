1a32768663047b38fca0940fa4c9389fd869e5cd
42KIM <ksahoo524@gmail.com>
2025-04-28 23:04:32 +0900

    색상변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           지원 이메일 주소:{" "}
           <a
             href="mailto:your-support-email@example.com"
-            className="text-blue-600 hover:underline"
+            className="text-blue-500 hover:underline"
           >
             bukubuku.dev@gmail.com
           </a>
